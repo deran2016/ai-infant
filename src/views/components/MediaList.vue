deb936307957083948747b5b7a75887b24ce8137
INYEOB <deran2016@gmail.com>
2021-09-24 14:45:44 +0900

    add quiz voices in MediaList

--- src/views/components/MediaList.vue
+++ src/views/components/MediaList.vue
@@ -38,6 +38,97 @@
         <p>Your browser does not support the <code>audio</code> element.</p>
       </audio>
     </div>
+
+    <!-- 퀴즈-선생님 -->
+    <div>
+      <audio
+        v-for="(file, key) in quiz"
+        :id="file + '-선생님'"
+        :key="key"
+        :src="require(`@/assets/quiz/${file}-선생님.mp3`)"
+        preload="auto"
+      >
+        <p>Your browser does not support the <code>audio</code> element.</p>
+      </audio>
+    </div>
+
+    <!-- 퀴즈-선생님(정답) -->
+    <div>
+      <audio
+        v-for="(file, key) in quiz"
+        :id="file + '-선생님(정답)'"
+        :key="key"
+        :src="require(`@/assets/quiz/${file}-선생님(정답).mp3`)"
+        preload="auto"
+      >
+        <p>Your browser does not support the <code>audio</code> element.</p>
+      </audio>
+    </div>
+
+    <!-- 퀴즈-유아 -->
+    <div>
+      <audio
+        v-for="(file, key) in quiz"
+        :id="file + '-유아'"
+        :key="key"
+        :src="require(`@/assets/quiz/${file}-유아.mp3`)"
+        preload="auto"
+      >
+        <p>Your browser does not support the <code>audio</code> element.</p>
+      </audio>
+    </div>
+
+    <!-- 퀴즈-유아(정답) -->
+    <div>
+      <audio
+        v-for="(file, key) in quiz"
+        :id="file + '-유아(정답)'"
+        :key="key"
+        :src="require(`@/assets/quiz/${file}-유아(정답).mp3`)"
+        preload="auto"
+      >
+        <p>Your browser does not support the <code>audio</code> element.</p>
+      </audio>
+    </div>
+
+    <!-- 퀴즈-유아선생님 -->
+    <div>
+      <audio
+        v-for="(file, key) in quiz"
+        :id="file + '-유아선생님'"
+        :key="key"
+        :src="require(`@/assets/quiz/${file}-유아선생님.mp3`)"
+        preload="auto"
+      >
+        <p>Your browser does not support the <code>audio</code> element.</p>
+      </audio>
+    </div>
+
+    <!-- 퀴즈-유아선생님(정답) -->
+    <div>
+      <audio
+        v-for="(file, key) in quiz"
+        :id="file + '-유아선생님(정답)'"
+        :key="key"
+        :src="require(`@/assets/quiz/${file}-유아선생님(정답).mp3`)"
+        preload="auto"
+      >
+        <p>Your browser does not support the <code>audio</code> element.</p>
+      </audio>
+    </div>
+
+    <!-- 효과음 -->
+    <div>
+      <audio
+        v-for="(file, key) in effect"
+        :id="file"
+        :key="key"
+        :src="require(`@/assets/quiz/${file}.mp3`)"
+        preload="auto"
+      >
+        <p>Your browser does not support the <code>audio</code> element.</p>
+      </audio>
+    </div>
   </div>
 </template>
 
@@ -61,6 +152,19 @@ export default {
       '잎 모양-유아선생님',
       '잎 모양-선생님 with Peer',
       '잎 모양-유아선생님 with Peer',
+    ],
+    quiz: [
+      '1번',
+      '2번',
+      '3번',
+      '4번',
+      '5번',
+    ],
+    effect: [
+      '맞았을 때(딩동댕)',
+      '틀렸을 때-선생님',
+      '틀렸을 때-유아',
+      '틀렸을 때-유아선생님',
     ],
   }),
 
